dbe88c58e0eb01cd269dd7e0b81f2ab3f2c6a896
Joseph Chung <joseph@salesbooster.io>
2020-09-17 15:55:34 -0400

    Update resizeMode to cover

--- src/libraries/TransformableImage/index.js
+++ src/libraries/TransformableImage/index.js
@@ -30,7 +30,7 @@ export default class TransformableImage extends PureComponent {
         enableScale: true,
         enableTranslate: true,
         imageComponent: undefined,
-        resizeMode: 'contain'
+        resizeMode: 'cover'
     };
 
     constructor (props) {
